7431226b3133648639575dbd638bacd2525580f0
João Rambo <jpramboguanabara@hotmail.com>
2022-08-15 13:55:01 -0300

    project done

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,4 +1,11 @@
-import { createContext, ReactNode, useState, useReducer } from 'react'
+import differenceInSeconds from 'date-fns/differenceInSeconds'
+import {
+  createContext,
+  ReactNode,
+  useState,
+  useReducer,
+  useEffect,
+} from 'react'
 import {
   ActionTypes,
   addNewCycleAction,
@@ -35,17 +42,43 @@ export default function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
   // useReducer recebe 2 parâmetros, uam função e o valor incial.
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  })
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    () => {
+      const storageStateAsJSON = localStorage.getItem(
+        '@pomodoro: cycles-state-1.0.0',
+      )
+
+      if (storageStateAsJSON) {
+        return JSON.parse(storageStateAsJSON)
+      }
+    },
+  )
 
   const { cycles, activeCycleId } = cyclesState
 
   const [isActiveCycle, setIsActiveCycle] = useState<boolean>(false)
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState<number>(0)
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState<number>(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate))
+    }
+
+    return 0
+  })
+
+  // salvar cycleState no local storage
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+
+    localStorage.setItem('@pomodoro: cycles-state-1.0.0', stateJSON)
+  }, [cyclesState])
 
   function resetCountOfSecondsHasPassed(seconds: number) {
     setAmountSecondsPassed(seconds)
